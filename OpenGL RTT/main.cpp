fa793768a1d4129f9784e87b6abf4f0a0da650bf
TheProphetOfRa <lelunedor@gmail.com>
2014-11-24 17:53:27 +0000

    Initial Commit

---
+++ OpenGL RTT/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL RTT
+//
+//  Created by David Hodgkinson on 24/11/2014.
+//  Copyright (c) 2014 David Hodgkinson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
