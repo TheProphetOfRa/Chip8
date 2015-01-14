11bf792d6de733c054954ee66cd051c76ba24cf8
TheProphetOfRa <lelunedor@gmail.com>
2015-01-14 23:04:09 +0000

    Added Visual Studio propject

---
+++ Project/VisualStudio/Chip8Emulator/Chip8Emulator/Shaders/array_vs.glsl
@@ -0,0 +1,12 @@
+#version 400
+
+layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec2 vertex_uv;
+
+out vec2 frag_uv;
+
+void main()
+{
+    frag_uv = vertex_uv;
+    gl_Position = vec4(vertex_position, 1.0f);
+}
